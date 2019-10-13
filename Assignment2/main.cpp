0d9e819775295cdba22a68102abf40cca52c8866
lesim <cjh4787@gmail.com>
2019-10-14 01:39:29 +0900

    no message

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -67,50 +67,7 @@ int main()
 	Sedan* sedan2 = new Sedan();
 	UBoat* uboat = new UBoat();
 
-	bool bAdded = sedan2->AddTrailer(new Trailer(50));
-	assert(bAdded);
-
-	bAdded = sedan2->AddTrailer(new Trailer(60));
-	assert(!bAdded);
-
-	bAdded = deusExMachina1->AddVehicle(airplane);
-	assert(bAdded);
-
-	deusExMachina1->AddVehicle(boat);
-	deusExMachina1->AddVehicle(boatplane);
-	deusExMachina1->AddVehicle(motorcycle);
-	deusExMachina1->AddVehicle(sedan);
-	deusExMachina1->AddVehicle(sedan2);
-	deusExMachina1->AddVehicle(uboat);
-	deusExMachina1->AddVehicle(new Airplane(5));
-	deusExMachina1->AddVehicle(new Airplane(5));
-	deusExMachina1->AddVehicle(new Airplane(5));
-
-	bAdded = deusExMachina1->AddVehicle(new Airplane(5));
-
-	assert(!bAdded);
-
-	deusExMachina1->RemoveVehicle(9);
-	deusExMachina1->RemoveVehicle(8);
-	bool bRemoved = deusExMachina1->RemoveVehicle(7);
-	assert(bRemoved);
-
-	bRemoved = deusExMachina1->RemoveVehicle(9);
-	assert(!bRemoved);
-	assert(0.00000000000000000000000000000000000000000000000000000000000001 > 0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001);
-	deusExMachina1->Travel(); // 모든 운송 수단이 이동
-	deusExMachina1->Travel(); // Boat, Motorcycle, 두 Sedan, UBoat가 이동
-	deusExMachina1->Travel(); // Motorcycle, 두 Sedan이 이동
-	deusExMachina1->Travel(); // Boat, Motorcycle, 두 Sedan이 이동
-	deusExMachina1->Travel(); // UBoat만 빼고 전부 이동
-	deusExMachina1->Travel(); // 어떤 운송 수단도 움직이지 않음
-	deusExMachina1->Travel(); // Boat, Motorcycle, 트레일러 안 달린 Sedan, UBoat가 이동
-	deusExMachina1->Travel(); // Boat, Motorcycle, 두 Sedan, UBoat가 이동
-	deusExMachina1->Travel(); // Airplane, Boatplane, Motorcycle, 두 Sedan이 이동
-	deusExMachina1->Travel(); // Boat, Motorcycle, 두 Sedan이 이동
-	deusExMachina1->Travel(); // Boat, Motorcycle, 두 Sedan이 이동
-	deusExMachina1->Travel(); // 트레일러 달린 Sedan만 이동
-	std::cout << deusExMachina1->GetFurthestTravelled()->GetName() << std::endl;
+	std::cout << deusExMachina1->GetFurthestTravelled() << std::endl;
 	//assert(deusExMachina1->GetFurthestTravelled() == boat);
 
 	return 0;
